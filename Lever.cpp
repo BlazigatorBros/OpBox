a27bc6ed4d235c75792d62201cfb918cae7a559e
ntopper <nicktopper@gmail.com>
2016-04-29 14:09:44 -0400

    implemented lever member functions

--- Lever.cpp
+++ Lever.cpp
@@ -2,4 +2,42 @@
 
 void Lever::setCarriage(bool state) {
 
+	go(state);
+	if(state) {
+		while(!outLimitState()) {}
+	}
+	else {
+		while(!inLimitState()) {}
+	}
+	stopCarriage();
+}
+
+void Lever::init() {
+	setCarriage(true);
+}
+
+
+bool Lever::inLimitState() {
+	return (!digitalRead(_inLimitPin));
+}
+
+bool Lever::outLimitState() {
+	return (!digitalRead(_outLimitPin));
+}
+
+
+
+void Lever::stopCarriage() {
+	digitalWrite(_motorPinOut, HIGH);
+	digitalWrite(_motorPinIn, HIGH);
+}
+
+void Lever::go(bool direction) {
+	stopCarriage();
+	if(direction) {
+		digitalWrite(_motorPinOut, LOW);
+	}
+	else {
+		digitalWrite(_motorPinIn, LOW);
+	}
 }
--- Lever.h
+++ Lever.h
@@ -7,17 +7,42 @@ class Lever: public Instrument {
 
         public:
                 Lever(int pin,
-                        int inMotor,
-                        int outMotor,
                         int inLimit,
                         int outLimit,
+                        int outMotor,
+                        int inMotor,
                         func_t isr)
                         : Instrument(pin, isr)
                 {
 
+                    _inLimitPin = inLimit;
+                    _outLimitPin = outLimit;
+                    _motorPinOut = outMotor;
+                    _motorPinIn = inMotor;
+                    pinMode(_motorPinOut, OUTPUT);
+                    pinMode(_motorPinIn, OUTPUT);
+                    pinMode(_leverPin, INPUT);
+                    pinMode(_inLimitPin, INPUT);
+                    pinMode(_outLimitPin, INPUT);
+                    stopCarriage();
+                    setCarriage(true);
                 }
 
                 void setCarriage(bool state);
+                bool outLimitState();
+                bool inLimitState();
+                void stopCarriage();
+                void init();
+
+        private:
+                void go(bool direction);
+                int _motorPinOut;
+                int _motorPinIn;
+                int _leverPin;
+                int _inLimitPin;
+                int _outLimitPin;
+                int _directionPin;
+                int _PWM_pin;
 };
 
 #endif
